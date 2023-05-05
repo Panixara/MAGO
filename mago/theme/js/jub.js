2631372f685f7dfbecd2346e21c9927af243e0dc
ppimpqx <93992468+ppimpqx@users.noreply.github.com>
2023-05-05 18:49:54 +0700

    Add files via upload

--- mago/theme/js/jub.js
+++ mago/theme/js/jub.js
@@ -6,7 +6,7 @@
  */
 
 const seach = () => {
-    const seachbox = document.getElementById("search-product").value.toUpperCase();
+    const seachbox = document.getElementById("search-item").value.toUpperCase();
     const storeitems = document.getElementById("product-list2")
     const product2 = document.querySelectorAll(".product2")
     const pname = storeitems.getElementsByTagName("h2")
@@ -14,10 +14,10 @@ const seach = () => {
     for(var i=0; i < pname.length; i++){
         let match = product2[i].getElementsByTagName('h2')[0];
 
-        if(match){
+        if(match) {
             let textvalue = match.textContent || match.innerHTML
 
-            if(textvalue.toUpperCase().indexOf(seachbox) > -1){
+            if (textvalue.toUpperCase().indexOf(seachbox) > -1) {
                 product2[i].style.display = "";
             } else {
                 product2[i].style.display = "none";
